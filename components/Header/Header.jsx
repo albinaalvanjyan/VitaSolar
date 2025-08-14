3c323ea4c50a43faccf3b6a71f38de6c61781627
Albina Alvanjyan <alvanjyanalbina@gmail.com>
2025-08-14 11:15:52 +0400

    changed header.jsx

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 import Button from '../Button/Button'
 import s from './Header.module.scss'
 import { useState } from 'react';
-
 function scrollToSection(id) {
     const section = document.getElementById(id);
     if (section) {
@@ -13,51 +12,46 @@ function scrollToSection(id) {
 
 const Header = () => {
     const [open, setOpen] = useState(false);
+    const [activeLink, setActiveLink] = useState("home");
+    const handleClick = (id) => {
+        setActiveLink(id);
+        scrollToSection(id);
+        history.pushState(null, "", `#${id}`);
+        setOpen(false);
+    };
+
     return (
         <header className={s.header}>
             <div className={`${s.header_content} container`}>
                 <div className="logo"><img src="./logo.png" alt="" /></div>
                 <div className={s.custom_menu}>
                     <nav>
-                        <NavLink to="/" className={({ isActive }) => isActive ? s.active : ""}>Home</NavLink>
-                        <NavLink to="/" onClick={(e) => { e.preventDefault(); scrollToSection("about"); }}>
-                            About Us
-                        </NavLink>
-                        <NavLink to="/" onClick={(e) => { e.preventDefault(); scrollToSection("solar-kits"); }}>
-                            Solar Kits
-                        </NavLink>
-                        <NavLink to="/" onClick={(e) => { e.preventDefault(); scrollToSection("calculator"); }}>
-                            Calculator
-                        </NavLink>
-                        <NavLink to="/" onClick={(e) => { e.preventDefault(); scrollToSection("our-works"); }}>
-                            Our Works
-                        </NavLink>
+                        <a className={activeLink === "home" ? s.active : ""} onClick={() => handleClick("home")}>Home</a>
+                        <a className={activeLink === "about" ? s.active : ""} onClick={() => handleClick("about")}>About Us</a>
+                        <a className={activeLink === "solar-kits" ? s.active : ""} onClick={() => handleClick("solar-kits")}>Solar Kits</a>
+                        <a className={activeLink === "calculator" ? s.active : ""} onClick={() => handleClick("calculator")}>Calculator</a>
+                        <a className={activeLink === "our-works" ? s.active : ""} onClick={() => handleClick("our-works")}>Our Works</a>
                     </nav>
                     <Button text="Call Us" backgroundColor={`var(--yellow)`} color={`var(--grey)`} />
                 </div>
                 <div className={s.mobile_menu}>
-                    <div className={s.menu} onClick={()=>setOpen(!open)}>{!open ? <i className='bx bx-menu'></i>:<i class='bx bx-x'></i>}</div>
+                    <div className={s.menu} onClick={() => setOpen(!open)}>
+                        {!open ? <i className='bx bx-menu'></i> : <i className='bx bx-x'></i>}
+                    </div>
                     <div className={`${s.menu_content} ${open ? s.active : ""}`}>
-                        <nav>
-                            <NavLink to="/" className={({ isActive }) => isActive ? s.active : ""}>Home</NavLink>
-                            <NavLink to="/" onClick={(e) => { e.preventDefault(); scrollToSection("about"); }}>
-                                About Us
-                            </NavLink>
-                            <NavLink to="/" onClick={(e) => { e.preventDefault(); scrollToSection("solar-kits"); }}>
-                                Solar Kits
-                            </NavLink>
-                            <NavLink to="/" onClick={(e) => { e.preventDefault(); scrollToSection("calculator"); }}>
-                                Calculator
-                            </NavLink>
-                            <NavLink to="/" onClick={(e) => { e.preventDefault(); scrollToSection("our-works"); }}>
-                                Our Works
-                            </NavLink>
-                        </nav>
+                    <nav>
+                        <a className={activeLink === "home" ? s.active : ""} onClick={() =>{ handleClick("home"); setOpen(false)}}>Home</a>
+                        <a className={activeLink === "about" ? s.active : ""} onClick={() => {handleClick("about"); setOpen(false)}}>About Us</a>
+                        <a className={activeLink === "solar-kits" ? s.active : ""} onClick={() => {handleClick("solar-kits"); setOpen(false)}}>Solar Kits</a>
+                        <a className={activeLink === "calculator" ? s.active : ""} onClick={() => {handleClick("calculator"); setOpen(false)}}>Calculator</a>
+                        <a className={activeLink === "our-works" ? s.active : ""} onClick={() => {handleClick("our-works"), setOpen(false)}}>Our Works</a>
+                    </nav>
                     </div>
                 </div>
             </div>
         </header>
     )
 }
+
 
 export default Header
